8b78a8afbb55c7173b14fe559f3dd0bfbb404f8b
Gregory Millare <gmillare4@gmail.com>
2021-04-20 12:19:17 -0500

    refactored guildId to exist in .env

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ require("dotenv").config();
 
 const client = new Discord.Client();
 
+const guildID = `${process.env.GUILD_ID}`;
+
 client.once("ready", () => {
   console.log("🐵 I AAAAM READY 🐱‍🚀");
 });
 
 client.on("message", (message) => {
-  if (message.guild.id === "692482578422300722") {
+  if (message.guild.id === guildID) {
     //console.log(message);
     if (message.content === "ping") {
       message.channel.send("pong");
