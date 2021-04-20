240ffbcf1ca6e54b1de03ee4f353b6a2948f97cc
Gregory Millare <gmillare4@gmail.com>
2021-04-20 11:51:29 -0500

    added emojis to node

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const client = new Discord.Client();
 
 client.once("ready", () => {
-  console.log("I AAAAM READY");
+  console.log("🐵 I AAAAM READY 🐱‍🚀");
 });
 
 client.login(process.env.BOT_TOKEN);
