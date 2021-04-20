151d8a2ccac06b801ac2456d09ce287f940948da
Gregory Millare <gmillare4@gmail.com>
2021-04-20 13:19:45 -0500

    added a referal to use the official gerg-bot channel if using the gerg command outside of the channel

--- src/commands/index.js
+++ src/commands/index.js
@@ -3,18 +3,24 @@ require("dotenv").config();
 const ping = require("./ping");
 
 const guildID = `${process.env.GUILD_ID}`;
+const channelID = `${process.env.CHANNEL_ID}`;
 
 const commands = {
   ping,
 };
 
 module.exports = async (message) => {
+  const args = message.content.split(" ");
+  if (args.length == 0 || args[0].toLowerCase() !== "gerg") return;
   if (message.guild.id === guildID) {
-    const args = message.content.split(" ");
-    if (args.length == 0 || args[0].toLowerCase() !== "gerg") return;
-    args.shift();
-    if (args[0] in commands) {
-      commands[args[0]](message, args);
+    if (message.channel.id === channelID) {
+      //console.log(message);
+      args.shift();
+      if (args[0] in commands) {
+        commands[args[0]](message, args);
+      }
+    } else {
+      await message.reply("Use the 'gerg-bot' channel you fuck");
     }
   }
 };
