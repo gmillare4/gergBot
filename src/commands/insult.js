af1f43978f6f0653cd808bbae1fd42453e9176b6
Gregory Millare <gmillare4@gmail.com>
2021-04-21 17:18:38 -0500

    insult command insults you if you don't @ someone properly

--- src/commands/insult.js
+++ src/commands/insult.js
@@ -13,11 +13,12 @@ const insults = [
 
 module.exports = async (message, args) => {
   if (args[0].toLowerCase() === "insult") {
+    const randomNum = Math.floor(Math.random() * 10);
     if (args[1][0] === "<") {
-      const randomNum = Math.floor(Math.random() * 10);
       await message.channel.send(`${args[1]}, ${insults[randomNum]}`);
     } else {
-      await message.channel.send("not a person idiot");
+      const authorId = message.author.id;
+      await message.channel.send(`<@${authorId}>, ${insults[randomNum]}`);
     }
   }
 };
