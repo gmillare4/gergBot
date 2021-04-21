72d8f4bd29adabaa3e7e8a787ca654b5cd72638f
Gregory Millare <gmillare4@gmail.com>
2021-04-21 17:51:14 -0500

    help command lists currently available commands and how to invoke them

--- src/commands/help.js
+++ src/commands/help.js
@@ -2,25 +2,26 @@ const Discord = require("discord.js");
 
 const exampleEmbed = new Discord.MessageEmbed()
   .setColor("#0099ff")
-  .setTitle("Some title")
-  .setURL("https://discord.js.org/")
+  .setTitle("List of Commands")
+  //   .setURL("https://discord.js.org/")
   .setAuthor(
-    "Some name",
-    "https://i.imgur.com/wSTFkRM.png",
-    "https://discord.js.org"
+    "GergBot",
+    "https://raw.githubusercontent.com/gmillare4/Audio-Visualizer/master/assets/audiovis%20headshot%20(1).png",
+    "https://github.com/gmillare4/gergBot"
   )
-  .setDescription("Some description here")
-  .setThumbnail("https://i.imgur.com/wSTFkRM.png")
+  //   .setDescription("Some description here")
+  //   .setThumbnail("https://i.imgur.com/wSTFkRM.png")
   .addFields(
-    { name: "Regular field title", value: "Some value here" },
-    { name: "\u200B", value: "\u200B" },
-    { name: "Inline field title", value: "Some value here", inline: true },
-    { name: "Inline field title", value: "Some value here", inline: true }
-  )
-  .addField("Inline field title", "Some value here", true)
-  .setImage("https://i.imgur.com/wSTFkRM.png")
-  .setTimestamp()
-  .setFooter("Some footer text here", "https://i.imgur.com/wSTFkRM.png");
+    { name: "Insult", value: "gerg insult <@Person>" },
+    { name: "Ping", value: "gerg ping" }
+    // { name: "\u200B", value: "\u200B" },
+    // { name: "Inline field title", value: "Some value here", inline: true },
+    // { name: "Inline field title", value: "Some value here", inline: true }
+  );
+//   .addField("Inline field title", "Some value here", true)
+//   .setImage("https://i.imgur.com/wSTFkRM.png")
+//   .setTimestamp()
+//   .setFooter("Some footer text here", "https://i.imgur.com/wSTFkRM.png");
 
 module.exports = async (message, args) => {
   if (args[0].toLowerCase() === "help") {
